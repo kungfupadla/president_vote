5d8e1596de03ad1a46e9c6bfc78a084862e47887
Dmitry Grebenshchikov <delise06@mail.ru>
2014-09-11 17:56:15 +0400

    add models & settings DB

--- president_vote/settings.py
+++ president_vote/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'president_vote.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'world_president',
+        'USER': 'world_president',
+        'PASSWORD': 'pos21kot',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
--- world_president/models.py
+++ world_president/models.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=150)
+    capital = models.ForeignKey(Capital)
+    president = models.ForeignKey(Presidents)
+    about_country = models.TextField()
+
+
+class Presidents(models.Model):
+    name = models.CharField(max_length=200)
+    biography = models.TextField()
+    photo = models.ImageField(verbose_name='Photo')
+
+class Capital(models.Model):
+    name = models.CharField(max_length=200)
