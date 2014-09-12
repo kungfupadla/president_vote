89fccfc64f5c7edeb98eb7d8f1d4a36553825d3a
Dmitry Grebenshchikov <delise06@mail.ru>
2014-09-12 14:40:59 +0400

    migrate to Django==1.7

--- world_president/admin.py
+++ world_president/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from world_president.models import Capital, Presidents, Country
 # Register your models here.
+
--- world_president/models.py
+++ world_president/models.py
@@ -2,19 +2,32 @@
 from django.db import models
 
 # Create your models here.
+class Capital(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+class Presidents(models.Model):
+    name = models.CharField(max_length=200)
+    biography = models.TextField()
+    photo = models.ImageField(upload_to="photos")
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    birthday = models.DateField(null=True)
+    def __unicode__(self):
+        return self.name
+
 class Country(models.Model):
     name = models.CharField(max_length=150)
     capital = models.ForeignKey(Capital)
     president = models.ForeignKey(Presidents)
     about_country = models.TextField()
+    flag = models.ImageField(upload_to='flags')
+
+    def __unicode__(self):
+        return self.name
 
 
-class Presidents(models.Model):
-    name = models.CharField(max_length=200)
-    biography = models.TextField()
-    photo = models.ImageField(verbose_name='Photo')
-
-class Capital(models.Model):
-    name = models.CharField(max_length=200)
 
 
